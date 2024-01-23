e067ada895e68f363bd4bbc4077f992bb89578bb
shalinipatel1621 <shalinipatel1621@gmail.com>
2024-01-23 07:16:31 +0000

    bestbuy

---
+++ src/main/java/com/bestbuy/constants/EndPoints.java
@@ -0,0 +1,18 @@
+package com.bestbuy.constants;
+
+
+public class EndPoints {
+
+    /**
+     * This is Endpoints of PRODUCTS
+     */
+    public static final String GET_ALL_PRODUCTS = "";
+    public static final String GET_SINGLE_PRODUCT_BY_ID = "/{productID}";
+    public static final String UPDATE_PRODUCT_BY_ID = "/{productID}";
+    public static final String DELETE_PRODUCT_BY_ID = "/{productID}";
+
+
+
+
+
+}
